004db2c90cf351743bce578140b43d4b78fce6ba
Otabek Tolqinov <otabektolqinov30@gmail.com>
2025-10-19 20:34:14 +0500

    Binary and Linear search implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] nums = {5, 7, 9, 11, 13};
+        int target = 13;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int result = BinarySearch.binarySearch(nums, target);
+        if (result != -1)
+            System.out.println("Element is found at index: " + result);
+        else
+            System.out.println("Element not found");
     }
 }
