65c297040ba92ae4f8b4c4cc4dcab730687db3a0
Otabek Tolqinov <otabektolqinov30@gmail.com>
2025-10-20 15:16:19 +0500

    Selection sort algo implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,14 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] nums = {5, 11, 9, 7, 13};
 
-        System.out.println("before sorting");
-        for (int num: nums){
-            System.out.print(num + ", ");
-        }
+        SelectionSort.sort(nums);
 
-        System.out.println();
-        BubbleSort.sort(nums);
-        System.out.println();
-
-        System.out.println("after sorting");
-        for (int num: nums){
-            System.out.print(num + ", ");
-        }
+        System.out.println(Arrays.toString(nums));
 
 
     }
