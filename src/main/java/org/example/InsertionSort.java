5854198af51150ec3ffff97ee92cb14822b16b34
Otabek Tolqinov <otabektolqinov30@gmail.com>
2025-10-21 10:54:37 +0500

    Insertion sort algo explained in my own way

--- src/main/java/org/example/InsertionSort.java
+++ src/main/java/org/example/InsertionSort.java
@@ -9,11 +9,14 @@ public class InsertionSort {
             int key = nums[i];
             int j = i - 1;
 
+            // todo: Main purpose of the while loop is to make space for key
+            // todo: after loop stops, index is at the right before the sport for the key
             while (j >= 0 && nums[j] > key){
                 nums[j + 1] = nums[j];
                 j--;
             }
 
+            // todo: that is why we add 1 for the spot that while finds
             nums[j + 1] = key;
         }
     }
